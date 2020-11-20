3813dc5e328313d6462e211bc223f6a92389f9e9
HyungjunAn <hyungjun0429@gmail.com>
2020-11-20 19:59:20 +0900

    cookie

--- README.md
+++ README.md
@@ -1,9 +1,21 @@
 # ONW
 - ONW is awesome game
 
+# Install
+```
+// node.js
+
+// packages
+$ npm install pm2 -g
+$ npm install -s cookie
+
+// Only Windows
+$ Set-ExecutionPolicy Unrestricted
+```
+
 # How to Run
 ## Server
 ```
-$ node main.js
+$ pm2 start main.js --watch
 ```
 ## Client
--- main.js
+++ main.js
@@ -1,21 +1,34 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var cookie = require('cookie');
 
 var app = http.createServer(function (request, response) {
   var _url = request.url;
+  var cookies = cookie.parse(request.headers.cookie);
 
   console.log(_url);
+  console.log(request.headers.cookie);
 
   if (_url == '/') {
     _url = '/login.html';
   }
+ 
   if (_url == '/favicon.ico') {
     response.writeHead(404);
     response.end();
     return;
   }
-  response.writeHead(200);
-  response.end(fs.readFileSync(__dirname + '/html/' + _url));
+  //response.writeHead(200);
+  //response.end(fs.readFileSync(__dirname + '/html/' + _url));
+  response.writeHead(200, {
+    'Set-Cookie':[
+      'yummy_cookie=choco',
+      'taste_cookie=strawberry',
+      `permanent=forever; Max-Age=3600`,
+      'secure=scure_value; Secure',
+    ]
+  });
+  response.end('Cookie!!'); 
 });
 app.listen(3000);
