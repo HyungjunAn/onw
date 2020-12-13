aa7c2cd104bbe1c7ef840c7c65a979dc71bbcf8e
HyungjunAn <hyungjun0429@gmail.com>
2020-12-14 00:15:05 +0900

    Add makeRoom, enterRoom method

--- main.js
+++ main.js
@@ -2,33 +2,63 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 var cookie = require('cookie');
+var qs = require('querystring');
 
-var app = http.createServer(function (request, response) {
-  var _url = request.url;
-  var cookies = cookie.parse(request.headers.cookie);
+var app = http.createServer(function(request, response) {
+    var _url = request.url;
 
-  console.log(_url);
-  console.log(request.headers.cookie);
+    //var cookies = cookie.parse(request.headers.cookie);
 
-  if (_url == '/') {
-    _url = '/login.html';
-  }
- 
-  if (_url == '/favicon.ico') {
-    response.writeHead(404);
-    response.end();
-    return;
-  }
-  //response.writeHead(200);
-  //response.end(fs.readFileSync(__dirname + '/html/' + _url));
-  response.writeHead(200, {
-    'Set-Cookie':[
-      'yummy_cookie=choco',
-      'taste_cookie=strawberry',
-      `permanent=forever; Max-Age=3600`,
-      'secure=scure_value; Secure',
-    ]
-  });
-  response.end('Cookie!!'); 
+    console.log(_url);
+    console.log(request.headers.cookie);
+
+    if (_url === '/') {
+        _url = '/login.html';
+        var flist = fs.readdirSync(__dirname + '/html/');
+        console.log(flist);
+        console.log(flist.length);
+        response.end(fs.readFileSync(__dirname + '/html/' + _url));
+        response.writeHead(200)
+    } else if (_url === '/game') {
+        var body = '';
+        var post = '';
+
+        request.on('data', function(data) {
+            body += data;
+            // Too much POST data, kill the connection!
+            // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+            if (body.length > 1e6) {
+                request.connection.destroy();
+            }
+        });
+
+        request.on('end', function () {
+            post = qs.parse(body);
+            console.log(post);
+
+            if (post.makeRoom !== undefined) {
+                response.end('Make Room!!');
+            } else {
+                response.end('Enter Room!!');
+            }
+        });
+
+        response.writeHead(200)
+    } else if (_url === 'cookie') {
+        response.end(fs.readFileSync(__dirname + '/html/' + _url));
+        response.writeHead(200, {
+            'Set-Cookie': [
+                'yummy_cookie=choco',
+                'taste_cookie=strawberry',
+                `permanent=forever; Max-Age=3600`,
+                //'secure=scure_value; Secure',
+            ]
+        });
+    } else {
+        response.writeHead(404);
+        response.end('Not found');
+        return;
+    }
 });
+
 app.listen(3000);
